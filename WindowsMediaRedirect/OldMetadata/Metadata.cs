92da8082511281cb34e9cb042241e3b0292263b8
etlam5123 <malte.kuhlmann96@gmail.com>
2021-10-11 18:33:51 +0200

    Projektdateien hinzufügen.

---
+++ WindowsMediaRedirect/OldMetadata/Metadata.cs
@@ -0,0 +1,258 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace WindowsMediaRedirect.OldMetadata {
+	[XmlRoot(ElementName = "track")]
+	public class Track {
+        private string wMContentID;
+        private string trackRequestID;
+        private string trackTitle;
+        private string uniqueFileID;
+        private string trackNumber;
+        private string trackPerformer;
+        private string trackComposer;
+        private string trackConductor;
+        private string period;
+        private string explicitLyrics;
+
+        [XmlElement(ElementName = "WMContentID")]
+        public string WMContentID {
+            get { return wMContentID; }
+            set { wMContentID = value; }
+        }
+
+        [XmlElement(ElementName = "trackRequestID")]
+        public string TrackRequestID {
+            get { return trackRequestID; }
+            set { trackRequestID = value; }
+        }
+
+        [XmlElement(ElementName = "trackTitle")]
+        public string TrackTitle {
+            get { return trackTitle; }
+            set { trackTitle = value; }
+        }
+
+        [XmlElement(ElementName = "uniqueFileID")]
+        public string UniqueFileID {
+            get { return uniqueFileID; }
+            set { uniqueFileID = value; }
+        }
+
+        [XmlElement(ElementName = "trackNumber")]
+        public string TrackNumber {
+            get { return trackNumber; }
+            set { trackNumber = value; }
+        }
+
+        [XmlElement(ElementName = "trackPerformer")]
+        public string TrackPerformer {
+            get { return trackPerformer; }
+            set { trackPerformer = value; }
+        }
+
+        [XmlElement(ElementName = "trackComposer")]
+        public string TrackComposer {
+            get { return trackComposer; }
+            set { trackComposer = value; }
+        }
+
+        [XmlElement(ElementName = "trackConductor")]
+        public string TrackConductor {
+            get { return trackConductor; }
+            set { trackConductor = value; }
+        }
+
+        [XmlElement(ElementName = "period")]
+        public string Period {
+            get { return period; }
+            set { period = value; }
+        }
+
+        [XmlElement(ElementName = "explicitLyrics")]
+        public string ExplicitLyrics {
+            get { return explicitLyrics; }
+            set { explicitLyrics = value; }
+        }
+    }
+
+	[XmlRoot(ElementName = "MDR-CD")]
+	public class MDRCD {
+        private string version;
+        private string mdqRequestID;
+        private string wMCollectionID;
+        private string wMCollectionGroupID;
+        private string uniqueFileID;
+        private string albumTitle;
+        private string albumArtist;
+        private string releaseDate;
+        private string label;
+        private string genre;
+        private string providerStyle;
+        private string publisherRating;
+        private string buyParams;
+        private string largeCoverParams;
+        private string smallCoverParams;
+        private string moreInfoParams;
+        private string dataProvider;
+        private string dataProviderParams;
+        private string dataProviderLogo;
+        private string needIDs;
+        private List<Track> track;
+
+        [XmlElement(ElementName = "version")]
+        public string Version {
+            get { return version; }
+            set { version = value; }
+        }
+
+        [XmlElement(ElementName = "mdqRequestID")]
+        public string MdqRequestID {
+            get { return mdqRequestID; }
+            set { mdqRequestID = value; }
+        }
+
+        [XmlElement(ElementName = "WMCollectionID")]
+        public string WMCollectionID {
+            get { return wMCollectionID; }
+            set { wMCollectionID = value; }
+        }
+
+        [XmlElement(ElementName = "WMCollectionGroupID")]
+        public string WMCollectionGroupID {
+            get { return wMCollectionGroupID; }
+            set { wMCollectionGroupID = value; }
+        }
+
+        [XmlElement(ElementName = "uniqueFileID")]
+        public string UniqueFileID {
+            get { return uniqueFileID; }
+            set { uniqueFileID = value; }
+        }
+
+        [XmlElement(ElementName = "albumTitle")]
+        public string AlbumTitle {
+            get { return albumTitle; }
+            set { albumTitle = value; }
+        }
+
+        [XmlElement(ElementName = "albumArtist")]
+        public string AlbumArtist {
+            get { return albumArtist; }
+            set { albumArtist = value; }
+        }
+
+        [XmlElement(ElementName = "releaseDate")]
+        public string ReleaseDate {
+            get { return releaseDate; }
+            set { releaseDate = value; }
+        }
+
+        [XmlElement(ElementName = "label")]
+        public string Label {
+            get { return label; }
+            set { label = value; }
+        }
+
+        [XmlElement(ElementName = "genre")]
+        public string Genre {
+            get { return genre; }
+            set { genre = value; }
+        }
+
+        [XmlElement(ElementName = "providerStyle")]
+        public string ProviderStyle {
+            get { return providerStyle; }
+            set { providerStyle = value; }
+        }
+
+        [XmlElement(ElementName = "publisherRating")]
+        public string PublisherRating {
+            get { return publisherRating; }
+            set { publisherRating = value; }
+        }
+
+        [XmlElement(ElementName = "buyParams")]
+        public string BuyParams {
+            get { return buyParams; }
+            set { buyParams = value; }
+        }
+
+        [XmlElement(ElementName = "largeCoverParams")]
+        public string LargeCoverParams {
+            get { return largeCoverParams; }
+            set { largeCoverParams = value; }
+        }
+
+        [XmlElement(ElementName = "smallCoverParams")]
+        public string SmallCoverParams {
+            get { return smallCoverParams; }
+            set { smallCoverParams = value; }
+        }
+
+        [XmlElement(ElementName = "moreInfoParams")]
+        public string MoreInfoParams {
+            get { return moreInfoParams; }
+            set { moreInfoParams = value; }
+        }
+
+        [XmlElement(ElementName = "dataProvider")]
+        public string DataProvider {
+            get { return dataProvider; }
+            set { dataProvider = value; }
+        }
+
+        [XmlElement(ElementName = "dataProviderParams")]
+        public string DataProviderParams {
+            get { return dataProviderParams; }
+            set { dataProviderParams = value; }
+        }
+
+        [XmlElement(ElementName = "dataProviderLogo")]
+        public string DataProviderLogo {
+            get { return dataProviderLogo; }
+            set { dataProviderLogo = value; }
+        }
+
+        [XmlElement(ElementName = "needIDs")]
+        public string NeedIDs {
+            get { return needIDs; }
+            set { needIDs = value; }
+        }
+
+        [XmlElement(ElementName = "track")]
+        public List<Track> Track {
+            get { return track; }
+            set { track = value; }
+        }
+    }
+
+	[XmlRoot(ElementName = "Backoff")]
+	public class Backoff {
+        private string time;
+
+        [XmlElement(ElementName = "Time")]
+        public string Time {
+            get { return time; }
+            set { time = value; }
+        }
+    }
+
+	[XmlRoot(ElementName = "METADATA")]
+	public class METADATA {
+        private MDRCD mDRCD;
+        private Backoff backoff;
+
+        [XmlElement(ElementName = "MDR-CD")]
+        public MDRCD MDRCD {
+            get { return mDRCD; }
+            set { mDRCD = value; }
+        }
+
+        [XmlElement(ElementName = "Backoff")]
+        public Backoff Backoff {
+            get { return backoff; }
+            set { backoff = value; }
+        }
+    }
+}
---
+++ WindowsMediaRedirect/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace WindowsMediaRedirect {
+    static class Program {
+        public static HttpListener listener;
+        public static string ip = "127.0.0.1";
+        public static int requestCount = 0;
+
+        public static string[] hosts = new string[] {
+            "redir.metaservices.microsoft.com",
+            "images.metaservices.microsoft.com",
+            "toc.music.metaservices.microsoft.com",
+            "windowsmedia.com",
+            "www.windowsmedia.com",
+            "services.windowsmedia.com"
+        };
+
+        public static void HandleIncomingConnections() {
+            while (true) {
+                HttpListenerContext ctx = listener.GetContext();
+
+                HttpListenerRequest req = ctx.Request;
+                HttpListenerResponse resp = ctx.Response;
+
+                Console.WriteLine("Request #: {0}", ++requestCount);
+                Console.WriteLine(req.Url.ToString());
+                Console.WriteLine(req.HttpMethod);
+                Console.WriteLine(req.UserHostName);
+                Console.WriteLine(req.UserAgent);
+                Console.WriteLine();
+
+                string target = "http://musicmatch-ssl.xboxlive.com/cdinfo/GetMDRCD.aspx" + req.Url.Query;
+
+                if (req.Url.ToString().StartsWith("http://windowsmedia.com/redir/GetMDRCD.asp") || req.Url.ToString().StartsWith("http://windowsmedia.com/redir/QueryTOC.asp")) {
+                    WebClient wc = new WebClient();
+                    wc.Encoding = System.Text.Encoding.UTF8;
+                    XmlSerializer newSerializer = new XmlSerializer(typeof(NewMetadata.METADATA));
+                    XmlSerializer oldSerializer = new XmlSerializer(typeof(OldMetadata.METADATA));
+
+                    string xmlin;
+                    byte[] data;
+
+                    try {
+                        xmlin = wc.DownloadString(target);
+                        StringReader reader = new StringReader(xmlin);
+                        NewMetadata.METADATA newmeta = (NewMetadata.METADATA)newSerializer.Deserialize(reader);
+
+                        StringWriter swriter = new StringWriter();
+                        oldSerializer.Serialize(XmlWriter.Create(swriter), NewToOldMeta(newmeta));
+                        data = Encoding.UTF8.GetBytes(swriter.ToString());
+                    } catch (Exception ex) {
+                        Console.WriteLine(ex.ToString());
+                        resp.StatusCode = 500;
+                        resp.Close();
+                        return;
+                    }
+
+                    resp.ContentType = "text/xml";
+                    resp.ContentEncoding = Encoding.UTF8;
+                    resp.ContentLength64 = data.LongLength;
+
+                    resp.OutputStream.Write(data, 0, data.Length);
+                } else if (req.Url.ToString().StartsWith("http://services.windowsmedia.com/cover/")) {
+                    string imgurl = req.Url.GetLeftPart(UriPartial.Path).Replace("http://services.windowsmedia.com/cover/", "http://musicimage.xboxlive.com/");
+                    WebClient wc = new WebClient();
+                    byte[] data;
+
+                    try {
+                        data = wc.DownloadData(imgurl);
+                    } catch (Exception ex) {
+                        Console.WriteLine(ex.ToString());
+                        resp.StatusCode = 500;
+                        resp.Close();
+                        return;
+                    }
+
+                    resp.ContentType = "image/jpeg";
+                    resp.ContentLength64 = data.LongLength;
+                    resp.OutputStream.Write(data, 0, data.Length);
+                } else if (req.Url.ToString().StartsWith("http://images.metaservices.microsoft.com/cover/")) {
+                    string imgurl = req.Url.GetLeftPart(UriPartial.Path).Replace("http://images.metaservices.microsoft.com/cover/https:/musicimage.xboxlive.com/", "http://musicimage.xboxlive.com/");
+                    resp.Redirect(imgurl);
+                } else {
+                    resp.Redirect(target);
+                }
+                resp.Close();
+            }
+        }
+
+        static OldMetadata.METADATA NewToOldMeta(NewMetadata.METADATA input) {
+            OldMetadata.METADATA output = new OldMetadata.METADATA();
+            output.MDRCD = new OldMetadata.MDRCD();
+            output.MDRCD.Version = "5.0";
+            output.MDRCD.MdqRequestID = input.MDRCD.MdqRequestID;
+            output.MDRCD.WMCollectionID = input.MDRCD.WMCollectionID;
+            output.MDRCD.WMCollectionGroupID = input.MDRCD.WMCollectionGroupID;
+            output.MDRCD.UniqueFileID = input.MDRCD.UniqueFileID;
+            output.MDRCD.AlbumTitle = input.MDRCD.AlbumTitle;
+            output.MDRCD.AlbumArtist = input.MDRCD.AlbumArtist;
+            output.MDRCD.ReleaseDate = input.MDRCD.ReleaseDate;
+            output.MDRCD.Label = input.MDRCD.Label;
+            output.MDRCD.Genre = input.MDRCD.Genre;
+            output.MDRCD.ProviderStyle = input.MDRCD.ProviderStyle;
+            output.MDRCD.PublisherRating = input.MDRCD.PublisherRating;
+            output.MDRCD.BuyParams = null;
+            output.MDRCD.LargeCoverParams = input.MDRCD.LargeCoverParams;
+            output.MDRCD.SmallCoverParams = null;
+            output.MDRCD.MoreInfoParams = input.MDRCD.MoreInfoParams;
+            output.MDRCD.DataProvider = input.MDRCD.DataProvider;
+            output.MDRCD.DataProviderParams = input.MDRCD.DataProviderParams;
+            output.MDRCD.DataProviderLogo = input.MDRCD.DataProviderLogo;
+            output.MDRCD.NeedIDs = input.MDRCD.NeedIDs;
+            output.MDRCD.Track = new List<OldMetadata.Track>();
+
+            if (output.MDRCD.LargeCoverParams != null)
+                output.MDRCD.LargeCoverParams = output.MDRCD.LargeCoverParams.Replace("https://musicimage.xboxlive.com/", "");
+
+            foreach (var trackin in input.MDRCD.Track) {
+                OldMetadata.Track trackout = new OldMetadata.Track();
+                trackout.WMContentID = trackin.WMContentID;
+                trackout.TrackRequestID = trackin.TrackRequestID;
+                trackout.TrackTitle = trackin.TrackTitle;
+                trackout.UniqueFileID = trackin.UniqueFileID;
+                trackout.TrackNumber = trackin.TrackNumber;
+                trackout.TrackPerformer = trackin.TrackPerformer;
+                trackout.TrackComposer = trackin.TrackComposer;
+                trackout.TrackConductor = trackin.TrackConductor;
+                trackout.Period = trackin.Period;
+                trackout.ExplicitLyrics = trackin.ExplicitLyrics;
+                output.MDRCD.Track.Add(trackout);
+            }
+            output.Backoff = new OldMetadata.Backoff();
+            output.Backoff.Time = input.Backoff.Time;
+            return output;
+        }
+
+        static void Main(string[] args) {
+            if (args.Length > 0) {
+                ip = args[0];
+            }
+
+            Console.WriteLine("WindowsMediaRedirect 1.0 - Make Windows Media Player Metadata services work again! Works on:");
+            Console.WriteLine("\n  Windows Media Player 9 Series\n  Windows Media Player 10\n  Windows Media Player 11\n  Windows Media Player 12 (Windows 7)\n");
+            Console.WriteLine("To change the listening address, add the desired IP as parameter. Example:");
+            Console.WriteLine("\n  WindowsMediaRedirect.exe 192.168.1.123");
+            Console.WriteLine("\nSet the following hosts entries if you have not done that yet:\n");
+            foreach (string host in hosts) {
+                Console.WriteLine(ip + "\t" + host);
+            }
+            listener = new HttpListener();
+            listener.Prefixes.Add("http://" + ip + ":80/");
+
+            try {
+                listener.Start();
+            } catch (Exception) {
+                Console.WriteLine("\nCould not bind connection for {0}", "http://" + ip + ":80/");
+                Console.WriteLine("\nTry running this application with admin or root privileges");
+                return;
+            }
+
+            Console.WriteLine("\nListening for connections on {0}", "http://" + ip + ":80/");
+
+            // Handle requests
+            HandleIncomingConnections();
+
+            // Close the listener
+            listener.Close();
+        }
+    }
+}
